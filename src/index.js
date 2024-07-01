c9ce2230ccb776b829963165080e5d0188d55f59
Lucca Limongi <laldf@cin.ufpe.br>
2024-07-01 14:24:31 -0300

    Removed #

--- src/index.js
+++ src/index.js
@@ -13,15 +13,19 @@ const RouteLogger = () => {
   return null;
 };
 
+const PathLogger = (data) => {
+  console.log(data)
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter basename={process.env.PUBLIC_URL}>
       <RouteLogger />
+      <PathLogger data={path} />
       <Routes>
-        <Route path="/#/kingcongue" element={<App showNorth={true} showSouth={false} />} />
-        <Route path="/#/deixaosgarotosbrincar" element={<App showNorth={false} showSouth={true} />} />
-        <Route path="/#/peixesibito" element={<App showNorth={true} showSouth={true} />} />
+        <Route path="/kingcongue" element={<App showNorth={true} showSouth={false} />} />
+        <Route path="/deixaosgarotosbrincar" element={<App showNorth={false} showSouth={true} />} />
+        <Route path="/peixesibito" element={<App showNorth={true} showSouth={true} />} />
         <Route path="/" element={<App showNorth={false} showSouth={false} />} />
       </Routes>
     </HashRouter>
