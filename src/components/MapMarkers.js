6831e83e50d1e8d66c3d21a0d31085fdc217e4d1
Lucca Limongi <luccalimongi@hotmail.com>
2025-05-31 07:27:39 -0300

    Fixed colors

--- src/components/MapMarkers.js
+++ src/components/MapMarkers.js
@@ -7,16 +7,28 @@ import { ICON_DICT, FACTION_DICT } from "../constants/Constants";
 const isBLUFOR = (faction) => ["Alemanha Ocidental", "Estados Unidos"].includes(faction);
 const isREDFOR = (faction) => ["Alemanha Oriental", "União Soviética", "Checoslováquia"].includes(faction);
 
-const getMarkerIconPath = (faction, icon, redNotBlue) => {
+const getMarkerIconPath = (faction, icon, fullShow, blueNotRed) => {
   let mappedColor;
   if (!isBLUFOR && !isREDFOR(faction)) {
     mappedColor = "black";
-  } 
-  else if (isBLUFOR(faction) && !redNotBlue) {
+  }
+  else if (isBLUFOR(faction) && fullShow) {
     mappedColor = "blue";
   }
-  else if (isREDFOR(faction) || redNotBlue) {
+  else if (isREDFOR(faction) && fullShow) {
     mappedColor = "red";
+  }
+  else if (blueNotRed && isREDFOR(faction)) {
+    mappedColor = "blue";
+  }
+  else if (blueNotRed && isBLUFOR(faction)) {
+    mappedColor = "red";
+  }
+  else if (!blueNotRed && isREDFOR(faction)) {
+    mappedColor = "red";
+  }
+  else if (!blueNotRed && isBLUFOR(faction)) {
+    mappedColor = "blue";
   }
 
   const mappedIcon = ICON_DICT[icon];
@@ -59,7 +71,7 @@ const MapMarkers = ({ markers, showMarkers, showBLUFOR, showREDFOR, getCoordinat
           key={index}
           position={getCoordinatesFromId(item.Coordenadas)}
           icon={L.icon({
-            iconUrl: getMarkerIconPath(item.Nacionalidade, item.Icone, (showREDFOR && !showBLUFOR)),
+            iconUrl: getMarkerIconPath(item.Nacionalidade, item.Icone, (showREDFOR && showBLUFOR), (showREDFOR && !showBLUFOR)),
             iconSize: [24, 24],
             iconAnchor: [12, 12],
             popupAnchor: [0, -24],
